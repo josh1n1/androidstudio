91b0ea9f9fc3306332f2cc00da9c4f29f9215a6e
Jubibani <strawberryloli3@gmail.com>
2024-09-08 00:59:19 +0800

    change outlined button background color to white

--- BasicsCodeLab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ BasicsCodeLab/app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.basicscodelab
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,26 +39,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-        )
+        Column (modifier = Modifier.padding(24.dp)){
+            for (name in names) {
+                Greeting(
+                    name = name,
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
+        }
+
     }
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(
         color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(24.dp)
+        modifier = Modifier.padding(2.5.dp)
         ) {
-        Text(
-            color = Color.Green,
-            modifier = Modifier.padding(24.dp),
-            text = "Hello $name!"
+        Row {
+            Column(modifier = Modifier.padding(24.dp).weight(1f)){
+                Text(
+                    color = Color.White,
+                    text = "Hello, "
 
-        )
+                )
+                Text(
+                    color = Color.White,
+                    text = "$name!"
+
+                )
+            }
+            OutlinedButton(
+                onClick = {},
+                colors = ButtonDefaults.outlinedButtonColors(
+                    containerColor = Color.White,  // Set the button background to white
+                    contentColor = Color.Black     // Set the text color to black
+                )
+                ) {
+                Text(
+                    text = "Show more",
+                )
+
+            }
+        }
     }
 }
 
@@ -58,7 +91,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BasicsCodeLabTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
 
